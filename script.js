21361ee0350dafec41ee685cd72ed9a644d1d593
Vice23 <mvice045@gmail.com>
2022-05-11 14:40:14 -0400

    added JS script file and connected it to the main HTML

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+//need to track the 'enter' -- event listenter on the form
+
+
+
+    //stop the page from refreshing when a form is submitted
+
+    //if they haven't entered a task but hits enter, they should get a warning
+
+    //when you add an item typed into the form, it is then added to the UL
+        //display the to do on the page within the ul (put it inside a list item) - create element and then add the text inside it? 
+        //include a checkbox within the li
+        
+
+//when you click on a task, you are able to toggle between checked vs. unchecked (done vs. not done)
+
+
+//BONUS
+//add a 'Reset" button, clearing all of the to dos
+// ad a 'remove task/ button to each task
+//add an 'edit task' button
+
+
+
+
